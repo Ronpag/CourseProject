aac7e00cdb0f2970ead762366d26ebb621bc31c8
LarSen <sh0tnickobra@gmail.com>
2026-06-19 13:39:52 +0300

    Added icon.ico

--- CRM/obj/Debug/net8.0-windows/CRM.AssemblyInfo.cs
+++ CRM/obj/Debug/net8.0-windows/CRM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97ddb9fc4134750cbfa79c15f9aaf0c6614db3a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a363fa6c870e2449a550a98b41b09b457cd595d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
