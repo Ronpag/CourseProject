4bfea7feb70364f39911b9618e26e0df68c33e3f
LarSen <sh0tnickobra@gmail.com>
2026-06-26 17:26:11 +0300

    Changed direction of data classes

--- CRM/obj/Debug/net8.0-windows/CRM.AssemblyInfo.cs
+++ CRM/obj/Debug/net8.0-windows/CRM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f83448f14996049212a4f0838123b74b8c11cc1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88c67354650d69637ece67892e1eb543eb56f950")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
