c0d335ffd2ffe238b751fabc8049505049fe79a3
LarSen <sh0tnickobra@gmail.com>
2026-07-01 03:54:35 +0300

    Added visual

--- CRM/obj/Debug/net8.0-windows/CRM.AssemblyInfo.cs
+++ CRM/obj/Debug/net8.0-windows/CRM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97b7b3cea36464a516979fb80ed22903b3617662")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41e3a0c6f8693765bae964e4478a26509d0b8814")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
